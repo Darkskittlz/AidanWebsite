2623cf43271a8dd173c67e8a934777364db1e3b9
tneal <tneal@breezeline.com>
2024-07-10 04:25:37 -0400

    additional changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,38 +13,17 @@ export default async function Home() {
     .props('title,metadata.image,metadata.content');
   return (
     <>
-      <div className="w-full grid justify-center grid-cols-1">
-        <div key="listen" className="items-center flex flex-col smMobile:w-full">
-          <h1 className="text-center py-4 text-3xl uppercase"> Listen </h1>
-          <iframe className="rounded-xl" src="https://open.spotify.com/embed/artist/2w251FvFhXVbtcc3iwmfXV?utm_source=generator" width="500px" height="384px" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div key="watch" className="items-center flex flex-col smMobile:w-full">
-          <h1 className="text-center py-4 text-3xl uppercase"> Watch </h1>
-          <iframe width="500px" height="384px" src="https://www.youtube.com/embed/d1NSCg0M1FQ" title="TAKE YOU THERE Official Music Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
-          <button className="border mt-4 border-blue rounded-full desktop:w-1/3 smMobile:w-1/4 justify-center cursor-pointer p-6 text-white target:shadow-lg bg-blue">
-            <a target="_blank" href="https://www.youtube.com/@aidanpaulmusic">Youtube Channel</a>
-          </button>
-        </div>
-        <div key="Shop" className="items-center flex flex-col smMobile:w-full">
-          <h1 className="text-center py-4 text-3xl uppercase"> Support </h1>
-          <img className="h-96 object-center rounded-lg" src="/bandcamp.png" />
-          <button className="border mt-4 border-blue rounded-full desktop:w-1/3 smMobile:w-1/4 justify-center cursor-pointer p-6 text-white target:shadow-lg bg-blue">
-            <a target="_blank" href="https://aidanpaul.bandcamp.com/track/alone-again-on-christmas-day"> BandCamp </a>
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col justify-center mt-12">
-        <h1 className="text-center text-5xl">Blog Posts</h1>
+      <div className="flex flex-col justify-center">
         {posts?.map(
           (post: {
             title: string,
             metadata: { image: { imgix_url: string }, content: string },
           }) => {
             return (
-              <div className="items-center pt-4 flex flex-col" key={post.title}>
-                <div className="smMobile:w-3/4 desktop:w-1/3 justify-center backdrop-blur-lg p-4 border rounded-xl border-white">
+              <div className="items-center pt-4 mx-2 flex flex-col" key={post.title}>
+                <div className="smMobile:w-full desktop:w-1/3 justify-center backdrop-blur-lg p-4">
                   <div>
-                    <h1 className="text-3xl text-center text-red">{post.title}</h1>
+                    <h1 className="text-3xl text-center">{post.title}</h1>
                   </div>
                   <div className="justify-center flex">
                     <img
@@ -59,6 +38,26 @@ export default async function Home() {
             );
           }
         )}
+      </div>
+      <div className="w-full grid justify-center grid-cols-1">
+        <div key="listen" className="items-center flex flex-col smMobile:w-full">
+          <h1 className="text-center py-4 text-3xl uppercase"> Listen </h1>
+          <iframe className="rounded-xl" src="https://open.spotify.com/embed/artist/2w251FvFhXVbtcc3iwmfXV?utm_source=generator" width="400px" height="384px" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+        <div key="watch" className="items-center flex flex-col smMobile:w-full">
+          <h1 className="text-center py-4 text-3xl uppercase"> Watch </h1>
+          <iframe width="400px" height="384px" className="rounded-xl" src="https://www.youtube.com/embed/d1NSCg0M1FQ" title="TAKE YOU THERE Official Music Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+          <button className="border mt-4 border-blue rounded-xl desktop:w-1/3 smMobile:w-2/4 justify-center cursor-pointer p-6 text-white target:shadow-lg bg-blue">
+            <a target="_blank" href="https://www.youtube.com/@aidanpaulmusic">Youtube Channel</a>
+          </button>
+        </div>
+        <div key="Shop" className="items-center flex flex-col smMobile:w-full">
+          <h1 className="text-center py-4 text-3xl uppercase"> Support </h1>
+          <img className="h-96 object-center rounded-lg" src="/bandcamp.png" />
+          <button className="border mt-4 border-blue rounded-xl desktop:w-1/3 smMobile:w-2/4 justify-center cursor-pointer p-6 text-white target:shadow-lg bg-blue">
+            <a target="_blank" href="https://aidanpaul.bandcamp.com/track/alone-again-on-christmas-day"> BandCamp </a>
+          </button>
+        </div>
       </div>
     </>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -23,37 +23,37 @@ export default async function Header() {
     };
   };
   return (
-    <nav className="space-x-4 sticky top-0 bg-white/20 dark:bg-black/20 backdrop-blur-lg w-full z-[9999]">
-      <div className="flex w-full items-center md:container p-4">
-        <Link href="/">
-          <img
-            src={`${settings.metadata.dark_logo.imgix_url}?w=500&auto=format,compression`}
-            alt={settings.metadata.company}
-            className="h-12 rounded-lg m-auto hidden dark:block"
-          />
-        </Link>
-        <div className="flex items-center w-full justify-between ml-4">
-          <div className="flex gap-x-2 justify-center mb-6 lg:mb-0">
-            {settings.metadata.links.map((link: Link) => {
-              return (
-                <a href={link.url} key={link.url} target="_blank" rel="noreferrer">
-                  <img
-                    className="h-[26px] w-[26px] rounded-lg"
-                    src={`${link.icon.imgix_url}?w=500&auto=format,compression`}
-                    alt={link.company}
-                  />
-                </a>
-              );
-            })}
-          </div>
-          <div>
-            <NavMenu
-              query={{ type: "navigation-menus", slug: "header" }}
-              hasMobileMenu
-              className="flex"
+    <nav className="space-x-4 items-center py-2 sticky top-0 bg-white/20 dark:bg-black/20 backdrop-blur-lg w-full z-[9999]">
+      <div className="w-full grid grid-cols-3 ml-4">
+        <div className="gap-x-2 ml-4 flex flex-row items-center lg:mb-0">
+          {settings.metadata.links.map((link: Link) => {
+            return (
+              <a href={link.url} key={link.url} target="_blank" rel="noreferrer">
+                <img
+                  className="h-[26px] w-[26px] rounded-lg"
+                  src={`${link.icon.imgix_url}?w=500&auto=format,compression`}
+                  alt={link.company}
+                />
+              </a>
+            );
+          })}
+        </div>
+        <div className="">
+          <Link href="/">
+            <img
+              src={`${settings.metadata.dark_logo.imgix_url}?w=500&auto=format,compression`}
+              alt={settings.metadata.company}
+              className="h-12 rounded-lg m-auto hidden dark:block"
             />
-            <CheckOut className="ml-4" productPath={"/services"} />
-          </div>
+          </Link>
+        </div>
+        <div className="flex justify-end items-center px-4">
+          <NavMenu
+            query={{ type: "navigation-menus", slug: "header" }}
+            hasMobileMenu
+            className="flex"
+          />
+          <CheckOut className="ml-4" productPath={"/services"} />
         </div>
       </div>
     </nav >
