688c0e3b3c838ba815b05a028539c742d40be4dd
Darkskittlz <darkskiiittles@gmail.com>
2024-08-05 02:30:39 -0600

    trying to get form to workjk

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,61 +1,72 @@
 'use client';
 
 import { FormEvent } from 'react';
+import { useState } from 'react';
 
 export function ContactForm() {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
   const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const formDataObject: { [key: string]: string } = {};
-    formData.forEach((value, key) => {
-      formDataObject[key] = value.toString();
-    });
-    const urlEncodedData = new URLSearchParams(formDataObject).toString();
+    let urlEncodedData = '';
+    const entries = formData.entries();
+    for (let pair = entries.next(); !pair.done; pair = entries.next()) {
+      const [key, value] = pair.value;
+      if (urlEncodedData !== '') {
+        urlEncodedData += '&';
+      }
+      urlEncodedData += encodeURIComponent(key) + '=' + encodeURIComponent(value as string);
+    }
     await fetch('/__forms.html', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: urlEncodedData
     });
     // Success & error handling should come here
-  };
-  // Rest of your component code remains the same
+    setFormSubmitted(true)
+  }
 
   return (
     <div className="flex flex-col border rounded-lg p-4">
-      <form name="contact" action="/success" method="POST" data-netlify="true" onSubmit={handleFormSubmit}>
-        <input className="text-black" type="hidden" name="form-name" value="contact" />
-        <p>
-          <label htmlFor="firstName">
-            First Name:
-          </label> <br />
-          <input className="bg-white text-black" type="text" name="firstname" id="firstname" key="firstname" />
-          <br />
-          <label htmlFor="lastName">
-            Last Name:
-          </label> <br />
-          <input className="bg-white text-black" type="text" name="lastName" id="lastName" key="lastName" />
-        </p>
-        <p>
-          <label htmlFor="youremail">
-            Your Email:
-          </label> <br />
-          <input className="bg-white text-black" type="email" name="email" id="youremail" key="email" />
-        </p>
-        <p>
-          <label htmlFor="yourmessage">
-            Message:
-          </label> <br />
-          <textarea className="bg-white text-black" name="message" id="yourMessage" key="message"></textarea>
-        </p>
-        <p>
-          <button
-            className="w-full flex justify-center bg-blue rounded-lg p-2"
-            id="btn"
-            value="send"
-            type="submit"
-          > Send </button>
-        </p>
-      </form>
+      {formSubmitted ? (
+        <p>Form submitted successfully! Thank you.</p>
+      ) : (
+        <form name="ContactForm" action="/" method="POST" data-netlify="true" onSubmit={handleFormSubmit}>
+          <input className="text-black" type="hidden" name="form-name" value="contact" />
+          <p>
+            <label htmlFor="firstName">
+              First Name:
+            </label> <br />
+            <input className="bg-white text-black" type="text" name="firstName" id="firstname" key="firstname" />
+            <br />
+            <label htmlFor="lastName">
+              Last Name:
+            </label> <br />
+            <input className="bg-white text-black" type="text" name="lastName" id="lastName" key="lastName" />
+          </p>
+          <p>
+            <label htmlFor="youremail">
+              Your Email:
+            </label> <br />
+            <input className="bg-white text-black" type="email" name="email" id="email" key="email" />
+          </p>
+          <p>
+            <label htmlFor="message">
+              Message:
+            </label> <br />
+            <input className="bg-white text-black mb-4" name="message" id="message" type="text" key="message"></input>
+          </p>
+          <p>
+            <button
+              className="w-full flex justify-center bg-blue rounded-lg p-2"
+              id="btn"
+              value="send"
+              type="submit"
+            > Send </button>
+          </p>
+        </form>
+      )}
     </div>
   )
 }
